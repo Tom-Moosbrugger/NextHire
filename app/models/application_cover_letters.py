1803fc5696a8ddd6a8d2de22e49e8a5beab4359d
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-02-21 11:38:20 -0800

    added application cover letters model

--- app/models/application_cover_letters.py
+++ app/models/application_cover_letters.py
@@ -2,8 +2,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
 
-class ApplicationResume(db.Model):
-    __tablename__ = "application_resumes"
+class ApplicationCoverLetter(db.Model):
+    __tablename__ = "application_cover_letters"
 
     if environment == "production":
         __table_args__ = {"schema": SCHEMA}
@@ -14,15 +14,15 @@ class ApplicationResume(db.Model):
         db.ForeignKey(add_prefix_for_prod("applications.id", ondelete="CASCADE")),
         nullable=False,
     )
-    resume_url = db.Column(db.String(2083), nullable=False)
+    cover_letter_url = db.Column(db.String(2083), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.today)
     updated_at = db.Column(db.DateTime, default=datetime.today, onupdate=datetime.today)
 
-    application = db.relationship("Application", back_populates="resumes")
+    application = db.relationship("Application", back_populates="cover_letters")
 
     def to_dict(self):
         return {
             "id": self.id,
             "applicationId": self.application_id,
-            "resumeUrl": self.resume_url,
+            "resumeUrl": self.cover_letter_url,
         }
