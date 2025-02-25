11dddf1c2fcf8b2b6ef95918f390951effe041a1
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-02-24 20:19:59 -0800

    added PUT route ti applications and added seed data

--- app/api/application_routes.py
+++ app/api/application_routes.py
@@ -15,9 +15,78 @@ application_routes = Blueprint("applications", __name__)
 @application_routes.route("/<int:application_id>", methods=["PUT"])
 @login_required
 def update_application(application_id):
-        
+    form = ApplicationForm()
 
-    return { "message": "testing" }
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    edited_application = Application.query.get(application_id)
+
+    if edited_application is None:
+        return {"errors": "Application not found"}, 404
+
+    if edited_application.user_id != current_user.id:
+        return {"message": "Application must belong to the current user"}
+
+    if form.validate_on_submit():
+        # updating fields
+
+        edited_application.application_status = form.application_status.data
+        edited_application.company_name = form.company_name.data
+        edited_application.job_title = form.job_title.data
+        edited_application.application_deadline = form.application_deadline.data
+        edited_application.company_website = form.company_website.data or None
+        edited_application.job_details = form.job_details.data or None
+        edited_application.job_post_url = form.job_post_url.data or None
+        edited_application.submission_details = form.submission_details.data or None
+
+        # handling AWS changes
+
+        if form.cover_letter.data is not None:
+            # delete old cover letter
+
+            aws_delete = remove_file_from_s3(edited_application.cover_letter_url)
+
+            if aws_delete is not True:
+                return aws_delete
+
+            # add new cover letter
+            cover_letter = form.cover_letter.data
+
+            cover_letter.filename = get_unique_filename(cover_letter.filename)
+
+            upload = upload_file_to_s3(cover_letter)
+
+            if "url" not in upload:
+                return upload
+
+            edited_application.cover_letter_url = upload["url"]
+
+        if form.resume.data is not None:
+            # delete old resume
+
+            aws_delete = remove_file_from_s3(edited_application.resume_url)
+
+            if aws_delete is not True:
+                return aws_delete
+
+            # add new resume
+            resume = form.resume.data
+
+            resume.filename = get_unique_filename(resume.filename)
+
+            upload = upload_file_to_s3(resume)
+
+            if "url" not in upload:
+                return upload
+
+            edited_application.resume_url = upload["url"]
+
+        db.session.commit()
+
+        return {edited_application.id: edited_application.to_dict()}
+
+    return form.errors, 400
+
 
 @application_routes.route("/<int:application_id>", methods=["DELETE"])
 @login_required
@@ -112,4 +181,4 @@ def get_applications():
         Application.user_id == current_user.id
     ).all()
 
-    return { application.id: application.to_dict() for application in applications }
+    return {application.id: application.to_dict() for application in applications}
