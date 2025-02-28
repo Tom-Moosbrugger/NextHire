238d9ffbd81637ac302025ff14b85ac5a7d39495
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-02-28 09:54:50 -0800

    created and styled common question tile

--- react-vite/src/components/CommonQuestions/CommonQuestions.jsx
+++ react-vite/src/components/CommonQuestions/CommonQuestions.jsx
@@ -2,12 +2,16 @@ import { useSelector } from "react-redux";
 import { CreateCommonQuestion } from "../CommonQuestionForm";
 import OpenModalButton from "../OpenModalButton";
 import Loading from "../Loading";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useModal } from "../../context/Modal";
+import * as commonQuestionActions from "../../redux/commonQuestions";
 import "./CommonQuestions.css";
 
 const CommonQuestions = () => {
-  const commonQuestions = useSelector(state => state.commonQuestions);
+  const { setModalContent } = useModal();
+  const commonQuestions = useSelector(commonQuestionActions.getQuestionsArr);
 
-  if (!commonQuestions) return <Loading />
+  if (!commonQuestions) return <Loading />;
 
   return (
     <article className="common-questions">
@@ -18,6 +22,18 @@ const CommonQuestions = () => {
           id="create-common-question"
         />
       </header>
+      <section className="common-question-tiles">
+        {commonQuestions.map((commonQuestion) => (
+          <div key={commonQuestion.id} className="common-question-tile">
+            <p>
+              {commonQuestion.question.length > 90
+                ? `${commonQuestion.question.slice(0, 89)}...?`
+                : commonQuestion.question}
+            </p>
+            <FaRegTrashAlt className="delete-common-question" />
+          </div>
+        ))}
+      </section>
     </article>
   );
 };
--- react-vite/src/components/OpenModalButton/OpenModalButton.jsx
+++ react-vite/src/components/OpenModalButton/OpenModalButton.jsx
@@ -5,7 +5,8 @@ function OpenModalButton({
   buttonText, // text of the button that opens the modal
   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
   onModalClose, // optional: callback function that will be called once the modal is closed
-  id //optional: lets you assign id to button component
+  id, //optional: lets you assign id to button component
+  className
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
@@ -15,7 +16,7 @@ function OpenModalButton({
     if (typeof onButtonClick === "function") onButtonClick();
   };
 
-  return <button id={id} onClick={onClick}>{buttonText}</button>;
+  return <button className={className} id={id} onClick={onClick}>{buttonText}</button>;
 }
 
 export default OpenModalButton;
